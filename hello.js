f7da6cd7a7cc747800f01900fc52d269d3dda136
cagodheere <huud6234@gmail.com>
2024-02-25 16:21:22 +0000

    hello

---
+++ hello.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello Macalin";
+}
+
+function mySecondFunction() {
+  let anything = document.getElementById("fname").value;
+
+  alert(`Hello Your Name Is ${anything}`);
+}
